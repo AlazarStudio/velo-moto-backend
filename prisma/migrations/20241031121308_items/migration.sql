7e89c10465264bd86f488adf88490ee5d6a15094
MusaJin <mussak949@gmail.com>
2024-10-31 15:13:19 +0300

    migration

---
+++ prisma/migrations/20241031121308_items/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN     "front_suspension" TEXT,
+ADD COLUMN     "fuel_consumption" TEXT,
+ADD COLUMN     "fuel_tank_capacity" TEXT,
+ADD COLUMN     "ground_clearance" TEXT,
+ADD COLUMN     "max_speed" TEXT,
+ADD COLUMN     "motor" TEXT,
+ADD COLUMN     "power" TEXT,
+ADD COLUMN     "rear_suspension" TEXT,
+ADD COLUMN     "starting" TEXT,
+ADD COLUMN     "torque" TEXT,
+ADD COLUMN     "transmission" TEXT,
+ADD COLUMN     "wheelbase" TEXT;
